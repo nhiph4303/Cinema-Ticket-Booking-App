76a93151499373c03c146c59f412f87c2d4b468e
Phan-Ngoc-Hanh-Nhi <155942839+nhiph4303@users.noreply.github.com>
2025-08-25 00:13:06 +0700

    .

--- CinemaBookingApp/Back-end/CinemaBookingApp/CinemaBookingApp/obj/Release/net8.0/CinemaBookingApp.AssemblyInfo.cs
+++ CinemaBookingApp/Back-end/CinemaBookingApp/CinemaBookingApp/obj/Release/net8.0/CinemaBookingApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaBookingApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d17a0b22565c21bdec2e153820dc20e39ae1744")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaBookingApp")]
